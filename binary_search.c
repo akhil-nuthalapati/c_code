63a3ccfffe03e21d2851ba79ea3e99d44d56628b
akhil-nuthalapati <nuthalapativenkatasaiakhil@gmail.com>
2025-11-24 11:50:05 +0530

    updated

--- binary_search.c
+++ binary_search.c
@@ -1,33 +1,43 @@
 #include<stdio.h>
-int main()
-{
-  int st,mid,end,tar,n,i;
-  int found = 0;
-  printf("enter number of mem alloc : ");
-  scanf("%d",&n);
-  int a[n];
-  printf("enter elements in an asending order : \n");
-  for(i=0;i<n;i++)
-  {
-    scanf("%d",&a[i]);
-  }
-  printf("enter target : ");
-  scanf("%d",&tar);
-  st=0,end=n-1;
-  while(st<=end)
-  {
-   mid = (st + end)/2;
-   if(tar > a[mid])
-   st = mid + 1;
-   else if(tar < a[mid])
-   end = mid-1;
-   else
-   {found = 1; 
-    break;}
-  }
-  if(found)
-  printf("target found at : %d",mid+1);
-  else
-  printf("target not found.....");
+int binarysearch(int a[], int n, int tar) {
+    int st = 0, end = n - 1, mid;
+
+    while(st <= end) {
+        mid = (st + end) / 2;
+        if(a[mid] == tar)
+            return mid + 1;
+
+        else if(a[mid] > tar)
+            end = mid - 1;
+
+        else
+            st = mid + 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    int n, i, tar, pos;
+
+    printf("enter number of mem alloc : ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("enter elements in ascending order : \n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("enter target : ");
+    scanf("%d", &tar);
+
+    pos = binarysearch(a, n, tar);
+
+    if(pos != 0)
+        printf("Element found at position : %d\n", pos);
+    else
+        printf("Element not found\n");
+
     return 0;
 }
